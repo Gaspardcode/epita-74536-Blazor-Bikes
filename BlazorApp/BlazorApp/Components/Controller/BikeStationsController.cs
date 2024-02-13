a4b6933dc8afd0b1a6e6a7712a8cd46d5ef202e8
Gaspardcode <117646624+Gaspardcode@users.noreply.github.com>
2024-02-13 10:50:27 +0000

    Add files via upload

---
+++ BlazorApp/BlazorApp/Components/Controller/BikeStationsController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using static BlazorApp.Components.Pages.Bikes;
+/*
+namespace BlazorApp.Components.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BikeStationsController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+
+        public BikeStationsController(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            List<BikeStation> stations = new()
+            {
+
+
+                new BikeStation { number = 1,  contractName="Dublin", name="Stand 1", banking=false},
+                new BikeStation { number = 2,  contractName="Dublin", name="Stand 2", banking=false},
+                new BikeStation { number = 3,  contractName="Dublin", name="Stand 3", banking=false},
+            };
+
+            return Ok(stations.Select(station => _mapper.Map<BikeStationDto>(station)));
+
+        }
+    }
+}
+*/
---
+++ BlazorApp/BlazorApp/Components/Models/AutoMapperProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using static BlazorApp.Components.Pages.Bikes;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace DublinBikesMock.Models
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<BikeStation, BikeStationDto>();
+        }
+    }
+}
